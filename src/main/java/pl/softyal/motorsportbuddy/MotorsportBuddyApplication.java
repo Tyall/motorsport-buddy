745bbbc3c8af988554fab8f95e5f680d5a707d5c
Dawid Stefański <ddstefanski@gmail.com>
2024-05-18 01:43:29 +0200

    initial commit

---
+++ src/main/java/pl/softyal/motorsportbuddy/MotorsportBuddyApplication.java
@@ -0,0 +1,13 @@
+package pl.softyal.motorsportbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorsportBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorsportBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/softyal/motorsportbuddy/MotorsportBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.softyal.motorsportbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorsportBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
